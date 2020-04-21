eb722c96cc20a1506b6165ab4c4e8afa2f8cd015
Yogev Strauber <yogev.strauber@gmail.com>
2020-04-21 12:09:06 +0300

    filter the array so it wil have no undefiend
    some of contacts in a user contact list have no name and or no number
    we decieded to take those with name and number only

--- GlobalFunctions/getUserContacts.js
+++ GlobalFunctions/getUserContacts.js
@@ -4,19 +4,46 @@ const getUserContacts = async () => {
     const { status } = await Contacts.requestPermissionsAsync();
     if (status === 'granted') {
         const { data } = await Contacts.getContactsAsync();
+
         let contactsArray = [];
 
         data.forEach((contact) => {
             let contactObj = {};
-            contactObj.Name = `${contact.firstName} ${contact.lastName !== undefined ? contact.lastName : ''}`
-            contactObj.PhoneNumber = contact.phoneNumbers !== undefined ? contact.phoneNumbers[0].number : 'no-number'
-            contactsArray.push(contactObj);
+            contactObj.Name = `${contact.firstName} ${contact.lastName !== undefined ? contact.lastName : ''}`;
 
+            if (contactObj.Name !== undefined && contact.phoneNumbers !== undefined) { //only contacts with name and numbers will get in our array
+                contactObj.PhoneNumber = contact.phoneNumbers[0].number;
+                contactsArray.push(contactObj);
+            }
+            // if (contact.emails !== undefined) {
+            //     contactObj.Mail = contact.emails[0].email;              ---> MAKES A LOT OF NULLS IN THE SQL
+            // }                                                        
         })
-        console.log(
-            `contects length: `, contactsArray.length,
-            `contacts-list: `, contactsArray
-        );
+
+        contactsArray.forEach((c) => {
+            console.log(c);
+        });
+
+
+
+
+
+
+        // console.log(
+        //     `contects length: `, contactsArray.length,
+        //     `contacts-list: `, contactsArray
+        // );
+        //it looks like this:
+        /**
+         * contacts-list:  Array [
+                                    Object {
+                                        "Name": "Aviad Pardo",
+                                        "PhoneNumber": "0507394264",
+                                        "userEmail": "example@gmail,com",
+                                    },
+                                    Object {... 
+                                ]
+         */
 
     }
 
