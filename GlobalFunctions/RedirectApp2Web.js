e29245ef5c1d975025fd9d954336e22e6d47ecd5
OrelKarmi <57489571+OrelKarmi@users.noreply.github.com>
2020-08-02 17:14:28 +0300

    /

--- GlobalFunctions/RedirectApp2Web.js
+++ GlobalFunctions/RedirectApp2Web.js
@@ -3,21 +3,18 @@ import UpdateUserCordinates from './UpdateUserCordinates'
 
 const RedirectApp2Web = async (user) => {
 
-    const go2Browser = async (user) => {
-        if (user.UserName === user.PhoneNumber) {
-            WebBrowser.openBrowserAsync(
-                `http://proj.ruppin.ac.il/bgroup5/FinalProject/frontEnd/#/HomePage/${user.UserID}`
-            );
-        } else {
-            WebBrowser.openBrowserAsync(
-                `http://proj.ruppin.ac.il/bgroup5/FinalProject/frontEnd/#/HomePage/${user.UserID}`
-            );
-        }
-    }
-
     await UpdateUserCordinates(user.UserID);
-
-    await go2Browser(user);
+    if (user.UserName === user.PhoneNumber) {
+        console.log('if');
+        WebBrowser.openBrowserAsync(
+            `http://proj.ruppin.ac.il/bgroup5/FinalProject/frontEnd/#/UserProfile/${user.UserID}`
+        );
+    } else {
+        console.log('else');
+        WebBrowser.openBrowserAsync(
+            `http://proj.ruppin.ac.il/bgroup5/FinalProject/frontEnd/#/HomePage/${user.UserID}`
+        );
+    }
 
 
 
